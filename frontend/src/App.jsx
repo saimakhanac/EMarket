12937eff3a19fd12decc69197b756505836dbca1
saimakhanac <saimakhan0zy0@gmail.com>
2026-08-07 19:58:18 +0530

    react-icons, react-router-bootstrap, react-router-dom, routing, images added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,8 @@
 import { Container } from "react-bootstrap";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+// import HomeScreen from "./screens/HomeScreen";
+import { Outlet } from "react-router-dom";
 
 function App() {
   return (
@@ -8,10 +10,10 @@ function App() {
       <Header />
       <main className="py-3">
         <Container>
-          <h1>Welcome to EMarket</h1>
+          <Outlet />
         </Container>
       </main>
-      <Footer/>
+      <Footer />
     </>
   );
 }
--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,6 +1,6 @@
 import { Navbar, Nav, Container } from "react-bootstrap";
 import { FaShoppingCart, FaUser } from "react-icons/fa";
-// import logo from "../assets/Elogo.png";
+import { LinkContainer } from "react-router-bootstrap";
 import logo from "../assets/logo.png";
 
 const Header = () => {
@@ -8,27 +8,30 @@ const Header = () => {
     <header>
       <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
         <Container>
-          <Navbar.Brand href="/">
+          <LinkContainer to='/'>
+          <Navbar.Brand>
             {/* <img
               style={{ width: "40px", height: "auto", marginRight: "8px" }}
               src={logo}
               alt="EMarket"
             /> */}
-            <img
-              src={logo}
-              alt="EMarket"
-            />
+            <img src={logo} alt="EMarket" />
             EMarket
           </Navbar.Brand>
+          </LinkContainer>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="ms-auto">
-              <Nav.Link href="/cart">
+              <LinkContainer to='/cart'>
+              <Nav.Link>
                 <FaShoppingCart /> Cart
               </Nav.Link>
-              <Nav.Link href="/login">
+              </LinkContainer>
+              <LinkContainer to='/login'>
+              <Nav.Link>
                 <FaUser /> Sign In
               </Nav.Link>
+              </LinkContainer>
             </Nav>
           </Navbar.Collapse>
         </Container>
