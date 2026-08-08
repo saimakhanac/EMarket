ebd8f0e531349eec6b8a4e639d490aa6654e0dc0
saimakhanac <saimakhan0zy0@gmail.com>
2026-08-09 00:45:45 +0530

    frontend: product screen, backend : package.json in root , express server, initial product route

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -11,11 +11,13 @@ import "../src/assets/styles/bootstrap.custom.css";
 import "../src/assets/styles/index.css";
 import App from "./App.jsx";
 import HomeScreen from "./screens/HomeScreen.jsx";
+import ProductScreen from "./screens/ProductScreen.jsx";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<App />}>
       <Route index={true} path="/" element={<HomeScreen />} />
+      <Route path="/product/:id" element={<ProductScreen />} />
     </Route>,
   ),
 );
