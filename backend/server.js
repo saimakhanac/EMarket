49a276377f6ff427096cb8bc3a5e84f43e48788a
saimakhanac <saimakhan0zy0@gmail.com>
2026-08-09 22:09:51 +0530

    install nodemon and concurrently

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,9 @@ import express from "express";
 import products from "./data/products.js";
 const port = 5000;
 const app = express();
+app.get("/", (req, res) => {
+  res.send("API is running...");
+});
 app.get("/api/products", (req, res) => {
   res.json(products);
 });
