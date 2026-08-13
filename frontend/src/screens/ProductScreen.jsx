a6e894cdfe5a5fa2a3c19e5ce70b7e7f01646e14
saimakhanac <saimakhan0zy0@gmail.com>
2026-08-14 00:30:20 +0530

    Loader and message component

--- frontend/src/screens/ProductScreen.jsx
+++ frontend/src/screens/ProductScreen.jsx
@@ -13,6 +13,8 @@ import {
   Row,
 } from "react-bootstrap";
 import Rating from "../components/Rating";
+import Loader from "../components/Loader";
+import Message from "../components/Message";
 
 const ProductScreen = () => {
   const { id: productId } = useParams();
@@ -28,12 +30,13 @@ const ProductScreen = () => {
       <Link className="btn btn-light my-3" to="/">
         Go Back
       </Link>
+
       {isLoading ? (
-        <h2>Loading...</h2>
+        <Loader />
       ) : error ? (
-        <div>
-          {error?.data?.message} || {error?.error}
-        </div>
+        <Message variant="danger">
+          {error?.data?.message || error.error}
+        </Message>
       ) : (
         <>
           <Row>
