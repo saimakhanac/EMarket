d2fa6faf80ebd1de96419e5bfad81d1799e5f6d0
saimakhanac <saimakhan0zy0@gmail.com>
2026-08-14 02:56:00 +0530

    delete button

--- frontend/src/screens/CartScreen.jsx
+++ frontend/src/screens/CartScreen.jsx
@@ -12,7 +12,7 @@ import {
 import { FaTrash } from "react-icons/fa";
 import Message from "../components/Message";
 import { useDispatch, useSelector } from "react-redux";
-import { addToCart } from "../slices/cartSlice";
+import { addToCart, removeFromCart } from "../slices/cartSlice";
 
 const CartScreen = () => {
   const navigate = useNavigate();
@@ -21,6 +21,12 @@ const CartScreen = () => {
   const { cartItems } = cart;
   const addToCartHandler = async (product, qty) => {
     dispatch(addToCart({ ...product, qty }));
+  };
+  const removeFromCartHandler = async (id) => {
+    dispatch(removeFromCart(id));
+  };
+  const checkOutHandler = () => {
+    navigate("/login?redirect=/shipping");
   };
   return (
     <div>
@@ -60,7 +66,11 @@ const CartScreen = () => {
                       </Form.Control>
                     </Col>
                     <Col md={2}>
-                      <Button type="button" variant="light">
+                      <Button
+                        type="button"
+                        variant="light"
+                        onClick={() => removeFromCartHandler(item._id)}
+                      >
                         <FaTrash />
                       </Button>
                     </Col>
@@ -88,6 +98,7 @@ const CartScreen = () => {
                   type="button"
                   className="btn-block"
                   disabled={cartItems.length === 0}
+                  onClick={checkOutHandler}
                 >
                   Proceed To Checkout
                 </Button>
