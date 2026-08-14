e670f21f3fc9bbcea88bfd038379b5fdf017b6b4
saimakhanac <saimakhan0zy0@gmail.com>
2026-08-15 04:31:44 +0530

    user Profile endpoints and bug fix of removed next() as per mongoose update

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -29,9 +29,9 @@ const userSchema = new mongoose.Schema(
 userSchema.methods.matchPassword = async function (enteredPassword) {
   return await bcrypt.compare(enteredPassword, this.password);
 };
-userSchema.pre("save", async function (next) {
+userSchema.pre("save", async function () {
   if (!this.isModified("password")) {
-    next();
+    return;
   }
   const salt = await bcrypt.genSalt(10);
   this.password = await bcrypt.hash(this.password, salt);
