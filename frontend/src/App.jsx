69d1a8a8d63fab626a57ba4879920afce2bdebeb
saimakhanac <saimakhan0zy0@gmail.com>
2026-08-17 16:21:35 +0530

    Login Functionality and toaster added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,8 @@
 import { Container } from "react-bootstrap";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 // import HomeScreen from "./screens/HomeScreen";
 import { Outlet } from "react-router-dom";
 
@@ -14,6 +16,7 @@ function App() {
         </Container>
       </main>
       <Footer />
+      <ToastContainer /> 
     </>
   );
 }
